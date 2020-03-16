55de7455edd3e5347001e74a7b011acecbf51f1e
ismetnl <44272237+ismetnl@users.noreply.github.com>
2020-03-16 15:03:15 +0300

    Create bubble-selection-sort.py

---
+++ bubble-selection-sort.py
@@ -0,0 +1,34 @@
+# bubble sort
+
+def bubble_sort(list_1):
+    n = len(list_1)
+
+    for i in range(n, 0, -1):
+
+        for j in range(n):
+
+            if list_1[j] > list_1[j + 1]:
+                t = list_1[j + 1]
+                list_1[j + 1] = list_1[j]
+                list_1[j] = t
+
+
+return list_1
+
+
+# selection sort
+
+def selection_sort(list_1):
+    n = len(list_1)
+
+    for i in range(n):
+
+        min = i
+        for j in range(i + 1, n):
+            if A[min] > A[j]:
+                min = j
+
+        A[i], A[min] = A[min], A[i]
+
+
+return list_1
